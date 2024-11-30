998a06a6a740f680ce449c0c41a2cd3be6bacf8f
d1mspy <rom.dog20@gmail.com>
2024-11-30 20:08:44 +0300

    create interface

--- bond_007/run.py
+++ bond_007/run.py
@@ -1,9 +1,11 @@
 import pygame
 from game import Game
 from db.connect import check_table
+from interface import interface
 
 # функция запуска
 if __name__ == "__main__":
+    interface()
     check_table()
     Game().run()
     pygame.quit()
